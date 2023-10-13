70c7c366727c2f6299ef76ba6fd3d1efb6067263
LucasFiorotto <129554123+LucasFiorotto@users.noreply.github.com>
2023-10-12 22:12:07 -0300

    Delete Attendi directory

--- Attendi/ABM_Alumno/baja_alumno.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once("../clases/Database.php");
-    $conexion = new Database;
-    $conexion->conectar();
-
-    $dniAlumno = $_GET['dni'];
-
-    $conexion->deleteAlumno($dniAlumno);
-?>
